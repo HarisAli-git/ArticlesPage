1f00dd9d5318f3d317b74a686fdc9c4d8393dc82
HarisAli-git <harisa73@gmail.com>
2022-01-24 21:53:29 +0500

    Added React-Router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Create from './Create';
 
 function App() {
-  const title = "Welcome to training!"
-  const likes = 50;
-  const link = "https://www.google.com";
 
   return (
+    <Router>
     <div className="App">
+      <Navbar />
       <div className="content">
-        <Navbar></Navbar>
-        <h1>{title}</h1>
-        <Home />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+        </Switch>
       </div>
     </div>
+    </Router>
   );
 }
 
