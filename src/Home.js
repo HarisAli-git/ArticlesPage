d0c879cbcc4868e48dedf4b1b449b59c29d16b55
HarisAli-git <harisa73@gmail.com>
2022-01-24 17:53:33 +0500

    Added an Array

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,11 @@ import { useState } from "react";
 const Home = () => {
     const [name, UpdateName] = useState('Mario');
     const [Age, UpdateAge] = useState(18);
+    const [Articles, ShowArticles] = useState([
+        {title: "Cars", body: "VolksWagon....", author: "NationalGeographic", id: 1},
+        {title: "PAF", body: "CAC/PAC JF-17 Thunder....", author: "Defence Front", id: 2},
+        {title: "Aeroplanes", body: "Boeing 777 300ER....", author: "Aerospace Indus.", id: 3},
+    ]);
 
     const clickEvent = (name, e) => {
         console.log("Button is Hit by: ", name, e);
@@ -24,6 +29,12 @@ const Home = () => {
                 }
             }>TestButton</button>
             <button onClick={TestEvent}>EventButton</button>
+            <div>{Articles.map((article) => (
+                <div className="article-desc" key={article.id}>
+                    <h1 className="title">{article.title}</h1>
+                    <p className="author"> Written By: {article.author}</p>
+                </div>
+            ))}</div>
         </div>
     );
 }
