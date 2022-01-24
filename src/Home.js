bcb15bdb40c5c4851de9b49f8caa91cb732e7ecf
HarisAli-git <harisa73@gmail.com>
2022-01-24 17:03:20 +0500

    Added events in buttons

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
 const Home = () => {
+    const clickEvent = (name, e) => {
+        console.log("Button is Hit by: ", name, e);
+    }
+
+    const TestEvent = (e) => {
+        console.log("Button is Hit by: ", e);
+    }
+     
     return (  
         <div className="home">
             <h1>HomePage</h1>
+            <button onClick={
+                (e) => {
+                    clickEvent("user", e);
+                }
+            }>TestButton</button>
+            <button onClick={TestEvent}>EventButton</button>
         </div>
     );
 }
