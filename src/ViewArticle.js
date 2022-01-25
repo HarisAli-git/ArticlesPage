041039325781c508c2c5df5e951321f68aa3f4a2
HarisAli-git <harisa73@gmail.com>
2022-01-25 15:42:43 +0500

    Added Delete Buttonn

--- src/ViewArticle.js
+++ src/ViewArticle.js
@@ -1,10 +1,21 @@
 import { useParams } from "react-router-dom";
 import useCustFetch from "./useCustFetch";
+import { useHistory } from 'react-router-dom'; 
 
 const ViewArticle = () => {
 
+    const history = useHistory();
     const { id } = useParams();
     const {data, isLoading, error} = useCustFetch('http://localhost:8000/articles/' + id);
+
+    const deleteArticle = () => {
+        fetch('http://localhost:8000/articles/' + id, {
+            method: 'DELETE'
+        }).then(
+            console.log('Article Deleted!'),
+            history.push('/')
+        )
+    } 
 
     return (  
         <div className="ViewArticle">
@@ -15,6 +26,8 @@ const ViewArticle = () => {
                 <h2>{data.title}</h2>
                 <p><strong>Author:</strong> {data.author}</p>
                 <div>{data.body}</div>
+                <button onClick={deleteArticle} 
+                >Delete Article!</button>
             </blog>)
             }
         </div>
