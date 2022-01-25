4b65b0da0960b7442a43f0491193dcb63a48fad5
HarisAli-git <harisa73@gmail.com>
2022-01-25 15:57:18 +0500

    Added Wrong-Route Errors

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './Home';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Create from './Create';
 import ViewArticle from './ViewArticle';
+import Error from './Error';
 
 function App() {
 
@@ -20,6 +21,9 @@ function App() {
           </Route>
           <Route path="/article/:id">
             <ViewArticle />
+          </Route>
+          <Route path="*">
+            <Error />
           </Route>
         </Switch>
       </div>
